5f5bc39b2e953d3c47ee79bd94ad985ebf87dea4
altontonn <altontonnalumasa@gmail.com>
2024-10-29 23:33:23 +0300

    Create the nav and header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'football-preview': "url(./image/football-preview.png)",
+      },
+    },
   },
   plugins: [],
-}
+};
